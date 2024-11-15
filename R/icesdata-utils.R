c4a165a79ec90469c5914f1d74a9b3ac911482f2
lauriekell <laurie@kell.es>
2024-11-15 14:52:06 +0000

    with kobe

--- R/icesdata-utils.R
+++ R/icesdata-utils.R
@@ -357,6 +357,19 @@ tryIt<-function(x){
   if ("try-error"%in%is(rtn)) return(NULL)
   return(rtn)}
 
+tryIt<-function(x){
+  rtn=try(x)
+  if ("try-error"%in%is(rtn)) return(NULL)
+  return(rtn)}
+
+kobeFn<-function(x){
+  names(attributes(x)$eqsim)    =tolower(names(attributes(x)$eqsim))
+  names(attributes(x)$benchmark)=tolower(names(attributes(x)$benchmark))
+  
+  FLQuants(x, "stock"  =function(x) ssb(x)%/%eqsim(      x)["bmsy"],
+           "harvest"=function(x) fbar(x)%/%benchmark(x)["fmsy"])}
+
 setGeneric('kobe',  function(path,method,...) standardGeneric('kobe'))
 setMethod( 'kobe',  signature(path='FLStock',method="missing"), 
-           function(path,method) {kobeFn(path)})
+           function(path,method) {tryIt(kobeFn(path))})
+
