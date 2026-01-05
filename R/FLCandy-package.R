516065389db6ec002976304ecf4f60d7f12b556d
lauriekell <laurie@kell.es>
2026-01-05 13:05:41 +0000

    dll

---
+++ R/FLCandy-package.R
@@ -0,0 +1,12 @@
+#' FLCandy Package
+#'
+#' @description 
+#' Candidate Methods for FLR - Contains prototypes and experimental methods for the FLR framework
+#'
+#' @docType package
+#' @name FLCandy-package
+#' @aliases FLCandy
+#' @useDynLib FLCandy, .registration = TRUE
+#' @keywords internal
+"_PACKAGE"
+
